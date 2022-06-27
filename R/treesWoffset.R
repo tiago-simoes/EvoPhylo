6decc8f34ba422e8fa9142de80d070fd8518b6ae
tiago-simoes <trsimoes87@gmail.com>
2022-06-27 16:26:40 -0400

    Housekeeping changes #2 (06-27-2022)

--- R/treesWoffset.R
+++ R/treesWoffset.R
@@ -1,7 +1,7 @@
 #' Convert trees produced by a BEAST2 FBD analysis with offset to trees with correct ages,
 #' accounting for possible metadata on the tips.
-#' 
-#' This method adds a dummy tip at the present (t = 0) to fully extinct trees with offsets, 
+#'
+#' This method adds a dummy tip at the present (t = 0) to fully extinct trees with offsets,
 #' in order to have correct ages (otherwise the most recent tip is assumed to be at 0).
 #' This is a workaround to get the proper ages of the trees into other tools such as TreeAnnotator.
 #'
@@ -16,56 +16,56 @@
 #' # Convert trees with offset to trees with dummy tip
 #' trees_file = system.file("extdata", "ex_offset.trees", package = "EvoPhylo")
 #' log_file = system.file("extdata", "ex_offset.log", package = "EvoPhylo")
-#' converted.trees = offset.to.dummy.metadata(trees_file, log_file)
-#' 
+#' converted_trees = offset.to.dummy.metadata(trees_file, log_file)
+#'
 #' # Do something with the converted trees - for instance, calculate the MCC summary tree
 #' # Then remove the dummy tip from the MCC tree
-#' mcc.tree = drop.dummy(system.file("extdata", "ex_offset.MCC.tre", package = "EvoPhylo"))
-#' 
+#' mcc_tree = drop.dummy(system.file("extdata", "ex_offset.MCC.tre", package = "EvoPhylo"))
+#'
 #' @export
 #' @importFrom stats median
 #' @seealso [offset.to.dummy()] (faster version discarding metadata)
 #' @md
-offset.to.dummy.metadata = function(trees.file, log.file, output.file = NULL, 
+offset.to.dummy.metadata = function(trees.file, log.file, output.file = NULL,
                                     dummy.name = "dummy") {
   trees = treeio::read.beast(trees.file)
   log = read.table(log.file, header = T)
   log = log$offset
   print(median(log))
-  
+
   for(i in 1:length(trees)) trees[[i]]@phylo$offset = log[i]
-  
+
   presenttrees = lapply(trees, function(tmp) {
     ntips = length(tmp@phylo$tip.label)
     root = ntips + 2
-    
+
     t = offset.to.dummy.phylo(tmp@phylo, dummy.name = dummy.name)
-    
+
     tmp@data$node = as.numeric(tmp@data$node)
     tmp@data$node[which(tmp@data$node > ntips)] = tmp@data$node[which(tmp@data$node > ntips)]+1
     tmp@data$node[which(tmp@data$node >= root)] = tmp@data$node[which(tmp@data$node >= root)]+1
     tmp@data$node = as.character(tmp@data$node)
-    
+
     metas = colnames(tmp@data)
     metas = metas[metas != "node"]
     new.data = list(node = as.character(ntips + 1))
     for(m in metas) new.data[[m]] = 0
     tmp@data = rbind(tmp@data, new.data)
-    
+
     tmp@phylo = t
     return(tmp)
   })
-  
+
   if(!is.null(output.file)) write.beast.treedatas(presenttrees, file = output.file)
   presenttrees
 }
 
 #' Convert trees produced by a BEAST2 FBD analysis with offset to trees with correct ages.
-#' 
-#' This method adds a dummy tip at the present (t = 0) to fully extinct trees with offsets, 
+#'
+#' This method adds a dummy tip at the present (t = 0) to fully extinct trees with offsets,
 #' in order to have correct ages (otherwise the most recent tip is assumed to be at 0).
 #' This is a workaround to get the proper ages of the trees into other tools such as TreeAnnotator.
-#' 
+#'
 #' **NB:** Any metadata present on the tips will be discarded. If you want to keep metadata (such as clock rate values),
 #' use \code{offset.to.dummy.metadata} instead.
 #'
@@ -80,35 +80,35 @@ offset.to.dummy.metadata = function(trees.file, log.file, output.file = NULL,
 #' # Convert trees with offset to trees with dummy tip
 #' trees_file = system.file("extdata", "ex_offset.trees", package = "EvoPhylo")
 #' log_file = system.file("extdata", "ex_offset.log", package = "EvoPhylo")
-#' converted.trees = offset.to.dummy.metadata(trees_file, log_file)
-#' 
+#' converted_trees = offset.to.dummy.metadata(trees_file, log_file)
+#'
 #' # Do something with the converted trees - for instance, calculate the MCC summary tree
 #' # Then remove the dummy tip from the MCC tree
-#' mcc.tree = drop.dummy(system.file("extdata", "ex_offset.MCC.tre", package = "EvoPhylo"))
-#' 
+#' mcc_tree = drop.dummy(system.file("extdata", "ex_offset.MCC.tre", package = "EvoPhylo"))
+#'
 #' @export
 #' @importFrom stats median
 #' @seealso [offset.to.dummy.metadata()] (slower version, keeping metadata)
 #' @md
-offset.to.dummy = function(trees.file, log.file, output.file = NULL, 
+offset.to.dummy = function(trees.file, log.file, output.file = NULL,
                            dummy.name = "dummy") {
   trees = ape::read.nexus(trees.file)
   log = read.table(log.file ,header = T)
   log = log$offset
-  
+
   for(i in 1:length(trees)) trees[[i]]$offset = log[i]
-  
+
   presenttrees = lapply(trees, function(t) {
     offset.to.dummy.phylo(t, dummy.name = dummy.name)
   })
-  
+
   if(!is.null(output.file)) ape::write.nexus(presenttrees, file = output.file)
   presenttrees
 }
 
 
 #' Remove dummy tip from tree, accounting for metadata on the tips
-#' 
+#'
 #' This method is designed to remove the dummy tip added on offset trees once postprocessing is
 #' complete (for instance once the summary tree has been built using TreeAnnotator).
 #'
@@ -122,35 +122,35 @@ offset.to.dummy = function(trees.file, log.file, output.file = NULL,
 #' @examples
 #' # Analyze the trees with dummy tips - for instance, calculate the MCC summary tree
 #' # Then remove the dummy tip from the MCC tree
-#' mcc.tree = drop.dummy(system.file("extdata", "ex_offset.MCC.tre", package = "EvoPhylo"))
-#' 
+#' mcc_tree = drop.dummy(system.file("extdata", "ex_offset.MCC.tre", package = "EvoPhylo"))
+#'
 #' @export
 drop.dummy = function(tree.file, output.file = NULL, dummy.name = "dummy", convert.heights = TRUE) {
   tmp = treeio::read.beast(tree.file)
-  
+
   tip = which(tmp@phylo$tip.label == dummy.name)
   tipn = which(tmp@data$node == as.character(tip))
   node = tmp@phylo$edge[which(tmp@phylo$edge[,2] == tip),1]
   noden = which(tmp@data$node == as.character(node))
-  
+
   offset = min(tmp@data$height_median[which(as.numeric(tmp@data$node) <= length(tmp@phylo$tip.label) &
                                               as.numeric(tmp@data$node) != tip)])
-  
+
   tmp@phylo = ape::drop.tip(tmp@phylo, tip)
   tmp@data = tmp@data[-c(tipn,noden),]
   tmp@data$node = as.numeric(tmp@data$node)
   tmp@data$node[(tmp@data$node) > tip] = tmp@data$node[(tmp@data$node) > tip] - 1
   tmp@data$node[(tmp@data$node) > node] = tmp@data$node[(tmp@data$node) > node] - 1
   tmp@data$node = as.character(tmp@data$node)
-  
+
   if(convert.heights) {
     for(m in c("height_0.95_HPD", "height_range", "height_median", "height")) {
       tmp@data[[m]] = lapply(tmp@data[[m]], function(x) x - offset)
     }
   }
-  
+
   if(!is.null(output.file)) write.beast.treedatas(list(tmp), file = output.file)
-  
+
   list(tree = tmp, offset = offset)
 }
 
@@ -159,48 +159,48 @@ drop.dummy = function(tree.file, output.file = NULL, dummy.name = "dummy", conve
 offset.to.dummy.phylo = function(t, dummy.name = "dummy") {
   ntips = length(t$tip.label)
   totn = ntips + t$Nnode
-  
+
   times = ape::node.depth.edgelength(t)
   root_time = max(times) + t$offset
-  
+
   root = ntips+2
   t$edge[which(t$edge > ntips)] = t$edge[which(t$edge > ntips)]+1
   t$edge[which(t$edge >= root)] = t$edge[which(t$edge >= root)]+1
-  
+
   t$edge = rbind(c(root, root+1), c(root, ntips+1),t$edge)
   t$edge.length = c(0.1 , root_time, t$edge.length)
   t$tip.label = c(t$tip.label, dummy.name)
   t$Nnode = t$Nnode + 1
-  
+
   ntips = length(t$tip.label)
   totn = ntips + t$Nnode
-  
+
   return(t)
 }
 
 # adapted from treeio to handle list of trees instead of single trees
 write.beast.treedatas = function(treedatas, file = "", translate = TRUE, tree.name = "STATE"){
   cat("#NEXUS\n", file = file)
-  cat(paste("[R-package treeio, ", date(), "]\n\n", sep = ""), 
+  cat(paste("[R-package treeio, ", date(), "]\n\n", sep = ""),
       file = file, append = TRUE)
   N <- treeio::Ntip(treedatas[[1]])
-  
+
   obj <- lapply(treedatas, treeio::as.phylo)
   ntree <- length(obj)
   cat("BEGIN TAXA;\n", file = file, append = TRUE)
-  cat(paste("\tDIMENSIONS NTAX = ", N, ";\n", sep = ""), file = file, 
+  cat(paste("\tDIMENSIONS NTAX = ", N, ";\n", sep = ""), file = file,
       append = TRUE)
   cat("\tTAXLABELS\n", file = file, append = TRUE)
-  cat(paste("\t\t", obj[[1]]$tip.label, sep = ""), sep = "\n", 
+  cat(paste("\t\t", obj[[1]]$tip.label, sep = ""), sep = "\n",
       file = file, append = TRUE)
   cat("\t;\n", file = file, append = TRUE)
   cat("END;\n", file = file, append = TRUE)
   cat("BEGIN TREES;\n", file = file, append = TRUE)
-  
+
   if (translate) {
     cat("\tTRANSLATE\n", file = file, append = TRUE)
     obj <- ape::.compressTipLabel(obj)
-    X <- paste("\t\t", 1:N, "\t", attr(obj, "TipLabel"), 
+    X <- paste("\t\t", 1:N, "\t", attr(obj, "TipLabel"),
                ",", sep = "")
     X[length(X)] <- gsub(",", "", X[length(X)])
     cat(X, file = file, append = TRUE, sep = "\n")
@@ -217,15 +217,15 @@ write.beast.treedatas = function(treedatas, file = "", translate = TRUE, tree.na
       obj <- ape::.uncompressTipLabel(obj)
     }
   }
-  
+
   for(i in 1:ntree) {
     treedatas[[i]]@phylo <- obj[[i]]
     root.tag <- if (treeio::is.rooted(treedatas[[i]])) "= [&R] " else "= [&U] "
-    
+
     cat("\tTREE *", paste0(tree.name,"_",i-1), root.tag, file = file, append = TRUE)
-    cat(treeio:::write_beast_newick(treedatas[[i]], file = ""), "\n", sep = "", 
+    cat(treeio:::write_beast_newick(treedatas[[i]], file = ""), "\n", sep = "",
         file = file, append = TRUE)
   }
-  
+
   cat("END;\n", file = file, append = TRUE)
 }
--- _dev/make_datasets.R
+++ _dev/make_datasets.R
@@ -1,33 +1,49 @@
 # setwd ("E:/Git/EvoPhylo/")
 
+######### Morphologicl data matrix example file
+characters <- as.data.frame(ape::read.nexus.data("_dev/extdata/MrBayes/DataMatrix.nex"))
+save(characters, file = "data/characters.rda")
+
+######### MrBayes example files
+
 #Making posterior3p
-posterior3p <- combine_log("_dev/extdata/MultiClockTree/LogFiles3p/",
+posterior3p <- combine_log("_dev/extdata/MrBayes/MrBayes/MultiClockTree/LogFiles3p/",
                         burnin = .25, downsample = 10000)
 save(posterior3p, file = "data/posterior3p.rda")
 
 #Making posterior1p
-posterior1p <- combine_log("_dev/extdata/SingleClockTree/LogFiles1p/",
+posterior1p <- combine_log("_dev/extdata/MrBayes/SingleClockTree/LogFiles1p/",
                         burnin = .25, downsample = 10000)
 save(posterior1p, file = "data/posterior1p.rda")
 
-#Making characters
-characters <- as.data.frame(ape::read.nexus.data("_dev/extdata/DataMatrix.nex"))
-save(characters, file = "data/characters.rda")
-
 #Making tree3p
-tree3p <- treeio::read.mrbayes("_dev/extdata/MultiClockTree/Tree3p.t")
+tree3p <- treeio::read.mrbayes("_dev/extdata/MrBayes/MultiClockTree/Tree3p.t")
 save(tree3p, file = "data/tree3p.rda")
 
 #Making tree1p
-tree1p <- treeio::read.mrbayes("_dev/extdata/SingleClockTree/Tree1p.t")
+tree1p <- treeio::read.mrbayes("_dev/extdata/MrBayes/SingleClockTree/Tree1p.t")
 save(tree1p, file = "data/tree1p.rda")
 
 #making rate_table_clades_means3
 #same as get_clockrate_table(tree3p) with clades added
-rate_table_clades_means3 <- read.csv("_dev/extdata/MultiClockTree/RateTable_Means_Clades.csv")
+rate_table_clades_means3 <- read.csv("_dev/extdata/MrBayes/MultiClockTree/RateTable_Means_Clades.csv")
 save(rate_table_clades_means3, file = "data/rate_table_clades_means3.rda")
 
 #making rate_table_clades_means1
 #same as get_clockrate_table(tree1p) with clades added
-rate_table_clades_means1 <- read.csv("_dev/extdata/SingleClockTree/RateTable_Means_Clades.csv")
+rate_table_clades_means1 <- read.csv("_dev/extdata/MrBayes/SingleClockTree/RateTable_Means_Clades.csv")
 save(rate_table_clades_means1, file = "data/rate_table_clades_means1.rda")
+
+######### BEAST2 example files
+
+#Handling BEAST2 trees with offsets
+posterior_trees_offset <- treeio::read.beast("_dev/extdata/BEAST2/OffsetTrees/ex_offset.trees")
+save(posterior_trees_offset, file = "data/posterior_trees_offset.rda")
+
+posterior_log_offset <- read.table("_dev/extdata/BEAST2/OffsetTrees/ex_offset.log", header = TRUE)
+save(posterior_log_offset, file = "data/posterior_log_offset.rda")
+
+mcc_dummy <- treeio::read.beast("_dev/extdata/BEAST2/OffsetTrees/ex_offset.MCC.tre")
+save(mcc_dummy, file = "data/mcc_dummy.rda")
+
+
